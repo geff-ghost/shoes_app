e96102a498d3af87260bbc52ea9d82de3d6b81df
OdhiamboGeoffrey <geoffrey.ochieng.odhiambo03@gmail.com>
2025-09-05 11:34:14 +0300

    Added filters
    Used a ListView.Builder with Chip and GestureDetector to add filters below the heading and made it scrollable and clickable

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> filters = const ['All', 'Addidas', 'Puma', 'Nike', 'Bata'];
+  late String selectedFilter;
+
+  //sets the initial filter when the page is loaded
+  @override
+  void initState() {
+    super.initState();
+    selectedFilter = filters[0];
+  }
 
   @override
   Widget build(BuildContext context) {
     final border = OutlineInputBorder(
-      borderSide: BorderSide(
-        color: Color.fromRGBO(225, 225, 225, 1)
-      ),
-      borderRadius: BorderRadius.circular(50)
+      borderSide: BorderSide(color: Color.fromRGBO(225, 225, 225, 1)),
+      borderRadius: BorderRadius.circular(50),
     );
 
     return Scaffold(
@@ -19,11 +32,15 @@ class HomePage extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              //heading of the app
               Text(
                 'Shoes Collection',
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
               ),
+
               SizedBox(height: 20),
+
+              //the Texfield area
               TextField(
                 decoration: InputDecoration(
                   hintText: 'Search',
@@ -31,6 +48,45 @@ class HomePage extends StatelessWidget {
                   border: border,
                   focusedBorder: border,
                   enabledBorder: border,
+                ),
+              ),
+
+              //ListView builder containing the chips
+              SizedBox(
+                height: 120,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: filters.length,
+                  itemBuilder: (context, index) {
+                    final filter = filters[index];
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            selectedFilter = filter;
+                          });
+                        },
+                        child: Chip(
+                          backgroundColor: selectedFilter == filter 
+                            ? Theme.of(context).colorScheme.primary 
+                            : Color.fromRGBO(245, 247, 249, 1),
+                          side: BorderSide(
+                            color: const Color.fromRGBO(425, 247, 249, 1),
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadiusGeometry.circular(30),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 10,
+                          ),
+                          label: Text(filter),
+                          labelStyle: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    );
+                  },
                 ),
               ),
             ],
