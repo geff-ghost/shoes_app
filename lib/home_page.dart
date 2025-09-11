8f9f7fd0e0792628fa07232c3bb1e15b06fc895e
OdhiamboGeoffrey <geoffrey.ochieng.odhiambo03@gmail.com>
2025-09-11 14:58:12 +0300

    Bottom Navigation Bar
    Added the bottom navogation bar to move between the home page and the cart page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shoes_app/global_variables.dart';
-import 'package:shoes_app/product_card.dart';
-import 'package:shoes_app/product_details_page.dart';
+import 'package:shoes_app/cart_page.dart';
+import 'package:shoes_app/product_list.dart.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,129 +10,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final List<String> filters = const [
-    'All',
-    'Jordans',
-    'Puma',
-    'Nike',
-    'Bata',
-    'Sneakers',
-    'Ashion',
-    'Fila',
-    'Casual',
-  ];
-  late String selectedFilter;
-
-  //sets the initial filter when the page is loaded
-  @override
-  void initState() {
-    super.initState();
-    selectedFilter = filters[0];
-  }
+  int currentPage = 0;
+  List<Widget> pages = const [ProductList(), CartPage()];
 
   @override
   Widget build(BuildContext context) {
-    final border = OutlineInputBorder(
-      borderSide: BorderSide(color: Color.fromRGBO(225, 225, 225, 1)),
-      borderRadius: BorderRadius.circular(50),
-    );
-
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              //heading of the app
-              Text(
-                'Shoes Collection',
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-
-              SizedBox(height: 20),
-
-              //the Texfield area
-              TextField(
-                decoration: InputDecoration(
-                  hintText: 'Search',
-                  prefixIcon: Icon(Icons.search),
-                  border: border,
-                  focusedBorder: border,
-                  enabledBorder: border,
-                ),
-              ),
-
-              //ListView builder containing the chips
-              SizedBox(
-                height: 120,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: filters.length,
-                  itemBuilder: (context, index) {
-                    final filter = filters[index];
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                      child: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            selectedFilter = filter;
-                          });
-                        },
-                        child: Chip(
-                          backgroundColor: selectedFilter == filter
-                              ? Theme.of(context).colorScheme.primary
-                              : Color.fromRGBO(245, 247, 249, 1),
-                          side: BorderSide(
-                            color: const Color.fromRGBO(425, 247, 249, 1),
-                          ),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadiusGeometry.circular(30),
-                          ),
-                          padding: EdgeInsets.symmetric(
-                            horizontal: 20,
-                            vertical: 10,
-                          ),
-                          label: Text(filter),
-                          labelStyle: TextStyle(fontSize: 16),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-
-              //ListView builder of the products
-              Expanded(
-                child: ListView.builder(
-                  itemCount: products.length,
-                  itemBuilder: (context, index) {
-                    final product = products[index];
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return ProductDetailsPage(product: product);
-                            },
-                          ),
-                        );
-                      },
-                      child: ProductCard(
-                        title: product['title'] as String,
-                        price: product['price'] as double,
-                        image: product['imageUrl'] as String,
-                        backroungColor: index.isEven
-                            ? Color.fromRGBO(216, 240, 253, 1)
-                            : const Color.fromRGBO(245, 247, 249, 1),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
-        ),
+      body: IndexedStack(
+        index: currentPage,
+        children: pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        iconSize: 30,
+        onTap: (value) {
+          setState(() {
+            currentPage = value;
+          });
+        },
+        currentIndex: currentPage,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: ''),
+        ],
       ),
     );
   }
